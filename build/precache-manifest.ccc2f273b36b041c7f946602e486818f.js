a469ba03a5fef453be4752d24a03e21ac9f6fde1
mayl30 <mary.luong21@yahoo.com>
2022-07-16 22:12:00 -0400

    tried to add app.scss back

---
+++ build/precache-manifest.ccc2f273b36b041c7f946602e486818f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e810e25013701b58e390548caadbc3f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b76d30b8e20f89add56d",
+    "url": "/static/css/main.7e285f1d.chunk.css"
+  },
+  {
+    "revision": "32cae9b8b3a6fef790c7",
+    "url": "/static/js/2.40ad33d1.chunk.js"
+  },
+  {
+    "revision": "e0fb6e08ce74850b1ffcc19fc2497460",
+    "url": "/static/js/2.40ad33d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76d30b8e20f89add56d",
+    "url": "/static/js/main.72cd7a00.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.72cd7a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
